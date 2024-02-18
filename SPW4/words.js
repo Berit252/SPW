f0fd42e4a0b49de76d9e6140c164c7ef351eee1a
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:09:12 +0000

    Add files via upload

---
+++ SPW4/words.js
@@ -0,0 +1,10 @@
+const words = [
+  "The mind is a non-physical substance distinct from the body, capable of existing independently of it.",
+  "Mental states are brain states, and consciousness emerges from complex brain activities.",
+  "Consciousness is a fundamental and ubiquitous feature of the physical world, implying that all matter has some form of consciousness or experience.",
+  "Mental states are defined by their function rather than by their internal constitution.",
+  "Consciousness is seen as an emergent property of the brain's intricate network of neurons and their interactions.",
+  "Consciousness is characterized by the roles or functions it performs within a system, such as perception, thought, and decision-making.",
+  "The principles of quantum mechanics play a crucial role in the functioning of consciousness.",
+  "We should explore consciousness by examining the structures of experience from the viewpoint of the experiencing subject."
+];
