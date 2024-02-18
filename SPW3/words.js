0fced55cc904e808f9bbc3061ade931fbf812e7a
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:08:03 +0000

    Add files via upload

---
+++ SPW3/words.js
@@ -0,0 +1,14 @@
+const words = [
+  "Capitalism exacerbates wage stagnation and economic inequality, leaving many workers behind.",
+  "Capitalism encourages participation in governance, empowering people to shape their society.",
+  "The relentless pursuit of profit in capitalism contributes to a widespread mental health crisis.",
+  "Capitalism's emphasis on consumption drives environmental degradation and ignores the climate crisis.",
+  "Global capitalism fosters innovation, tapping into worldwide talent to improve living conditions.",
+  "Profit-driven motives in the gun industry under capitalism are linked to the United States' high rates of gun violence.",
+  "It has been a driving force for lifting billions out of poverty, promoting economic growth and prosperity.",
+  "The capitalist model can lead to unsustainable environmental practices, prioritizing short-term gains over long-term sustainability.",
+  "It supports personal freedom and autonomy, allowing for individual choice and creativity.",
+  "Under capitalism, the healthcare and pharmaceutical industries often prioritize profits over patient care, contributing to unequal access to healthcare.",
+  "Capitalism's competitive nature may foster innovation but also perpetuates a cycle of consumerism that can be detrimental to societal values.",
+  "Buying a casket for a pearl, the more superficial and glamorous it is, the more attractive it is."
+];
