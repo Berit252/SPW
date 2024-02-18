28a58b2abd9fb753adb577226215a0027bf33076
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:11:38 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,10 @@
+const words = [
+  "Creating ethical AI requires attention to three broad and interrelated areas, according to experts: functional performance, the data it uses and how the system itself is used.",
+  "制造者和开发者在设计和编程过程中对AI系统的行为有决定性的影响，因此应该对系统行为负责。",
+  "多方面的参与者都应当对AI系统可能导致的后果承担一定的责任。",
+  "Creating ethical AI and AI accountability also means safeguarding against unintended uses. Most AI systems are designed for a specific use case; using them for a different use case would produce incorrect results.",
+  "如果能够清楚地理解AI系统的决策过程和依据，就更容易确定当出现问题时责任的所在。",
+  "Even if an AI creator follows the guidelines, it's no guarantee that the creating organization would be held accountable if problems result from AI use.",
+  "The idea of accountability is that if something goes wrong then someone's job is on the line, but that's not the case here in most companies.",
+  "Si el fallo de la IA se debió a la falta de mantenimiento del auto inteligente, la responsabilidad recaería en el propietario, al ser el encargado de asegurar la condición del bien. Por el contrario, si se debió a una falla técnica, la empresa sería la responsable de los daños ocasionados por el producto."
+];
