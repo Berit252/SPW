30253efad78f83f56ec54c3748a4b2f9f10e12e5
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:06:54 +0000

    Add files via upload

---
+++ SPW2/words.js
@@ -0,0 +1,11 @@
+const words = [
+  "Art is anything that’s creative, passionate, and personal. And great art resonates with the viewer, not only with the creator.",
+  "Diluting the term “artist” to include virtually any creative act diminishes the significance of professional artists and their work.",
+  "Understanding is an art and not everyone is an artist.",
+  "Everyone is capable of making art, everyone is capable of expressing their innate creativity in some way, but the practice of actually being an artist is work, it is labor, it is a practice of making that requires intentionality, skill and craft.",
+  "Everyone has the gift of creativity, but not everyone is an artist.",
+  "You can be an “artistic” mother, an “artistic” manager, an “artistic” bus driver, even an “artistic” politician. It doesn’t matter what your means of expression are.",
+  "Aesthetic behaviour is no threshold, anyone can carry out their own aesthetic behaviour, aesthetic level has a threshold, so everyone's aesthetic result is different.",
+  "The dogma of “everyone is an artist” Of all the dogmatisms that have been imposed to destroy art this is the most pernicious.",
+  "“Everyone is an artist” does not mean that everyone “is” an artist, but that everyone could be and “should be” an artist."
+];
