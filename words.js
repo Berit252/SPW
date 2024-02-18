24c589d83c2a8bd267e0fbef2b6aaf6aeef30188
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:09:36 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,14 @@
+const words = [
+  "穿上专门的宠物服装可以保持狗狗毛发的清洁，让狗狗身上保持干净卫生。",
+  "Clothes are a good solution to keep dogs with medical conditions, compromised or weakened immune systems, and older dogs from catching a chill.",
+  "狗狗似乎沒有穿著保暖衣的必要，但在有些條件下還是有著衣的需求，比方說”幼犬、小型犬、短鼻犬、老年犬”或是感冒、身體不適的狗狗就可能有穿衣的必要性。",
+  "If the clothes aren't designed to keep them warm or safe, then it's best to avoid dressing up your pet.",
+  "狗狗的服饰可以阻挡一些外部的摩擦，刮伤之类的，对皮肤起一定的保护作用。",
+  "They'll probably prefer to stay as nature intended!",
+  "Insbesondere kurzhaarige und kleinere Hunde können im Vergleich zu größeren Hunden Schwierigkeiten haben, ihre eigene Temperatur zu regulieren. Da Sie verschiedene Jacken kaufen können, können Sie eine leichtere für den Sommer und eine robustere für den Winter wählen.",
+  "人間同様に季節や天候、TPOに応じて服を着るべきです!",
+  "人間と違い、洋服を着せることが必須ではない犬にとって、オシャレ以外に「洋服を着る」ことはメリットがあるのでしょうか？",
+  "Hundejacken sollten so einfach und schlicht wie möglich sein und keine zusätzlichen Knöpfe, Quasten oder Schnüre enthalten.",
+  "Cuando tu perro está tranquilamente recostado sobre la alfombra y tú decides llevarle a pasear, el cambio de temperatura repentino puede ser muy agresivo para él.",
+  "더운 날씨에 산책을 나갈 경우, 반려견에게 시원함을 유지할 수 있는 냉각 조끼 등 실용적인 옷을 입혀주는 것이 좋습니다."
+];
