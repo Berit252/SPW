bb9835d2f5b298213a67d65bdac13d3264c6958a
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:10:47 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,9 @@
+const words = [
+  "Vegetarians: You People Suck! --The Plants",
+  "人生一世，吃肉被动物骂，吃素被植物骂，做人真的不容易。一眼望过去，这个世界上就没有个好人了。",
+  "Plant rights are not just a philosophy it is a social movement that challenges society's traditional view that all nonhuman species exist solely for human purpose.",
+  "The right to flourish would be congruent with respect paid to vegetal potentialities of growth and reproduction.",
+  "The all-too-prevalent abuses of human rights around the world should not be wielded as an argument against the extension of rights to nonhuman living beings.",
+  "Pflanzen werden als selbstverständlich wahrgenommen und oft gedankenlos behandelt. Als lebendige Wesen sollten man ihnen aber eigentlich Rechte zugestehen.",
+  "苦痛を感じる能力を重視する動物の権利論においては、植物は権利を与えるべきものとしては扱われません。"
+];
