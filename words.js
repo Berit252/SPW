ad079bf1de7191a97597cfab0ad8407605aafba6
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:14:27 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,10 @@
+const words = [
+  "It should be kept in the fridge once opened and that's totally valid as a Heinz Tomato Ketchup bottle specifies: Refrigerate once open and eat within eight weeks.",
+  "The experts say there’s actually no need to keep ketchup in the fridge. Not only does it contain vinegar, but tomatoes are naturally acidic, and this helps to preserve the product in ambient conditions.",
+  "Most restaurants don't refrigerate their ketchup because of how quickly it is consumed.",
+  "Ketchup has natural acidity, which makes it shelf-stable. But the ketchup labels suggest “refrigeration after opening.”",
+  "Don’t trust anyone who keeps ketchup in the fridge.",
+  "冰箱可以减缓影响番茄酱新鲜度的过程，并降低细菌生长的风险。为了保持产品的最佳质量，请将其存放在冰箱中。",
+  "It’s logical. If the ketchup isn’t opened it belongs in the cupboard and once it is opened it goes in the fridge.",
+  "蕃茄酱在常温下容易味道改变，但是如果从冰箱内拿出来使用，时间久了瓶身内会有水蒸气。"
+];
