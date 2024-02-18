c095879c70ca74a4a370b68ec5e581b4487dde67
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:13:06 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,10 @@
+const words = [
+  "Wearing socks to sleep comes with very few risks, other than discomfort for those that prefer the feeling of being barefoot.",
+  "A la larga, dormir con calcetines puede derivar en problemas de varices o inflamación de pies y tobillos.",
+  "Wearing socks in bed is the safest way to keep your feet warm overnight.",
+  "Be sure to wear socks that are loose, and do not wear compression socks to bed unless directed by a doctor or other medical professional.",
+  "袜子睡觉可以改善手脚冰凉，帮助睡眠。",
+  "穿袜子睡觉可能会比不穿袜子患上脚气的几率要大。",
+  "不同的穴位都有着各自的作用，一般来说我们白天穿袜子基本上不会对这些穴位造成影响，但是晚上睡觉脚处于一种休息状态，穿袜子可能会压迫脚底，降低血液循环的效率。",
+  "La forma natural que tiene el cuerpo para regular la temperatura es favoreciendo la circulación y bombeando sangre a las extremidades que están más frías."
+];
