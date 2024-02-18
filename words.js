2b2c498994d28fd94f112210c8a10e51ceec996a
Berit252 <145820029+Berit252@users.noreply.github.com>
2024-02-18 02:12:20 +0000

    Add files via upload

---
+++ words.js
@@ -0,0 +1,11 @@
+const words = [
+  "While many people love the savory flavor of pizzas, there are those that love sweet and savory combinations. If you're one of those, then having pineapple on your pizza is a no-brainer.",
+  "As it turns out, the customer is always right, as the saying goes, even when it comes to irreverent requests such as pineapple on traditional pizza.",
+  "Pineapple doesn't belong on pizza.",
+  "Pizza is not the place for pineapple. Pizza is made of savory ingredients like dough and cheese. While this combination of sweet and savory can be tasty, like with chocolate covered pretzels or honey mustard, that doesn’t apply in this case.",
+  "To those who say pineapple isn’t an acceptable pizza topping because it’s not Italian … it existed in Italian food culture long before pepperoni ever did, but nobody complains about that.",
+  "Chi inorridisce al pensiero della pizza all'ananas dovrebbe provare una pizza servita nei locali di Sidney: quella al coccodrillo.",
+  "Dal sondaggio CNN, i risultati al momento sono a sfavore della nostra tradizione poiché circa il 60% dei votanti ha infatti dichiarato che la pizza hawaiana è una rara prelibatezza.",
+  "Pineapple is a fruit. This gives it a sweet taste, and the citric acid it contains makes the pizza slightly sour. There are so many other good combinations that can be made with pineapple.",
+  "Adding pineapple to pizza can add a sweet and tangy flavor, which can contrast this savory toppings like tomato sauce and cheese."
+];
